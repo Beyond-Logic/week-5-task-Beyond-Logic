e70ef416c8410f35d4ab82f4e92bf50e2036277d
Beyond-Logic <edemaero@gmail.com>
2021-04-27 15:04:36 +0100

    Simple WebServer #Java

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.StringTokenizer;
 
 public class WebServer implements Runnable {
 
     static final File WEB_ROOT = new File(".");
-    static final File HTML_FILE = new File("src/main/java/WebFiles/index.html");
-    static final File JSON_FILE = new File("src/main/java/WebFiles/student.json");
-    static  int PORT = 7070;
+    String HTML_FILE = ("src/main/java/WebFiles/index.html");
+    String JSON_FILE = ("src/main/java/WebFiles/conFig.json");
+    static  int PORT = 8080;
     static final boolean verbose = true;
     private final Socket connect;
 
@@ -53,53 +54,55 @@ public class WebServer implements Runnable {
             StringTokenizer parse = new StringTokenizer(input);
             String method = parse.nextToken().toUpperCase();
             fileRequested = parse.nextToken().toLowerCase();
+            String[] line = input.split(" ");
+            System.out.println(Arrays.toString(line));
 
             if(!method.equals("GET") && !method.equals("HEAD")){
                 if(verbose) {
                     System.out.println("Not Supported: " + method + "method.");
                 }
-                }
-                    else {
+            }
+            else {
 
-                    if(fileRequested.endsWith("/")){
-                        fileRequested += HTML_FILE;
-                    }
-
-                    else if (fileRequested.endsWith("/json/")){
-                        fileRequested += JSON_FILE;
-                    }
-
-
-
-                    File file = new File(WEB_ROOT, fileRequested);
-                    int fileLength = (int) file.length();
-                    String content = getContentType(fileRequested);
-
-                    if(method.equals("GET")) {
-                        byte[] fileData = readFileData(file, fileLength);
-                        out.println("HTTP/1.1 200 OKAY");
-                        out.println("Server: Web Server");
-                        out.println("Date: " + new Date());
-                        out.println("Content-type: " + content);
-                        out.println("Content-length: " + fileLength);
-                        out.println();
-                        out.flush();
-
-                        dataOut.write(fileData, 0, fileLength);
-                        dataOut.flush();
-
+                if(line[1].equals("/")){
+                    fileRequested = HTML_FILE;
                 }
 
-                    if(verbose) {
-                        System.out.println("\nStatus: Working\n");
-                        System.out.println("HTTP/1.1 200 OKAY");
-                        System.out.println("Server: Web Server");
-                        System.out.println("Date: " + new Date());
-                        System.out.println("File: " + fileRequested );
-                        System.out.println("Content-Type: " + content);
-                        System.out.println("Content-Length: " + fileLength);
+                else if (line[1].equals("/json")){
+                    fileRequested = JSON_FILE;
+                }
 
-            } }
+
+
+                File file = new File(WEB_ROOT,fileRequested);
+                int fileLength = (int) file.length();
+                String content = getContentType(fileRequested);
+
+                if(method.equals("GET")) {
+                    byte[] fileData = readFileData(file, fileLength);
+                    out.println("HTTP/1.1 200 OKAY");
+                    out.println("Server: Web Server");
+                    out.println("Date: " + new Date());
+                    out.println("Content-type: " + content);
+                    out.println("Content-length: " + fileLength);
+                    out.println();
+                    out.flush();
+
+                    dataOut.write(fileData, 0, fileLength);
+                    dataOut.flush();
+
+                }
+
+                if(verbose) {
+                    System.out.println("\nStatus: Working\n");
+                    System.out.println("HTTP/1.1 200 OKAY");
+                    System.out.println("Server: Web Server");
+                    System.out.println("Date: " + new Date());
+                    System.out.println("File: " + fileRequested );
+                    System.out.println("Content-Type: " + content);
+                    System.out.println("Content-Length: " + fileLength);
+
+                } }
         }
         catch (FileNotFoundException fileNotFoundException) {
             try {
@@ -108,59 +111,68 @@ public class WebServer implements Runnable {
                 System.out.println("Error with File not Found exception: " + ioe.getMessage());
             }
         } catch (IOException ioe) {
-                System.out.println("server error" + ioe );
-            } finally {
-                try {
-                    in.close();
-                    out.close();
-                    dataOut.close();
-                    connect.close();
-
-                } catch (Exception e) {
-                    System.out.println("Error Closing stream" + e.getMessage());
-                }
-
-                if (verbose) {
-                    System.out.println("\nConnection Closed");
-                }
-            }
-        }
-
-        private byte[] readFileData (File file, int fileLength) throws IOException {
-            FileInputStream fileIn = null;
-            byte[] fileData = new byte[fileLength];
+            System.out.println("server error" + ioe );
+        } finally {
             try {
-                fileIn = new FileInputStream(file);
-                fileIn.read(fileData);
+                in.close();
+                out.close();
+                dataOut.close();
+                connect.close();
 
-            } finally {
-                if (fileIn != null)
-                    fileIn.close();
+            } catch (Exception e) {
+                System.out.println("Error Closing stream" + e.getMessage());
             }
 
-            return fileData;
-        }
-
-
-
-            private String getContentType(String fileRequested){
-            if(fileRequested.endsWith(".htm") || fileRequested.endsWith(".html"))
-                return "text/html";
-            else if(fileRequested.endsWith(".css"))
-                return "text/css";
-            else if(fileRequested.endsWith(".json"))
-                return "application/json";
-            else if(fileRequested.endsWith(".js"))
-                return "text/javascript";
-            else return "text/plain";
+            if (verbose) {
+                System.out.println("\nConnection Closed");
             }
-
-
-            private void fileNotFound (String fileRequested) throws IOException {
-                System.out.println("404 Not Found");
-                if(verbose) {
-                    System.out.println("File: " + fileRequested + " not Found");
-                }
-
         }
     }
+
+    private byte[] readFileData (File file, int fileLength) throws IOException {
+        FileInputStream fileIn = null;
+        byte[] fileData = new byte[fileLength];
+        try {
+            fileIn = new FileInputStream(file);
+            fileIn.read(fileData);
+
+        } finally {
+            if (fileIn != null)
+                fileIn.close();
+        }
+
+        return fileData;
+    }
+
+
+
+    private String getContentType(String fileRequested){
+        if(fileRequested.endsWith(".htm") || fileRequested.endsWith(".html"))
+            return "text/html";
+        else if(fileRequested.endsWith(".css"))
+            return "text/css";
+        else if(fileRequested.endsWith(".json"))
+            return "application/json";
+        else if(fileRequested.endsWith(".js"))
+            return "text/javascript";
+        else if(fileRequested.endsWith(".ico"))
+            return "image/x-icon";
+        else if(fileRequested.endsWith(".png"))
+            return "image/png";
+        else if(fileRequested.endsWith(".jpg"))
+            return "image/jpg";
+        else if(fileRequested.endsWith(".mp3"))
+            return "audio/mpeg";
+        else return "text/plain";
+    }
+
+
+
+    private void fileNotFound (String fileRequested) throws IOException {
+        System.out.println("404 Not Found");
+        if(verbose) {
+            System.out.println("File: " + fileRequested + " not Found");
+        }
+
+    }
+}
--- src/test/java/WebServerTest.java
+++ src/test/java/WebServerTest.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.StringTokenizer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,13 +10,13 @@ class WebServerTest {
 
         final File WEB_ROOT = new File(".");
         final File HTML_FILE = new File("src/main/java/WebFiles/index.html");
-        final File JSON_FILE = new File("src/main/java/WebFiles/student.json");
-        int PORT = 7070;
+        final File JSON_FILE = new File("src/main/java/WebFiles/conFig.json");
+        int PORT = 8080;
         final boolean verbose = true;
         final Socket connect = new Socket();
 
-        assertEquals(7070, PORT);
-        assertNotEquals(8080, PORT);
+        assertEquals(8080, PORT);
+        assertNotEquals(7070, PORT);
 
         assertEquals(WEB_ROOT, WEB_ROOT);
         assertNotEquals(HTML_FILE, WEB_ROOT);
