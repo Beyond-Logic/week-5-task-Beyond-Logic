680febf3d54644558c395333e5a0e577da6d80ff
Beyond-Logic <edemaero@gmail.com>
2021-04-28 15:00:42 +0100

    Simple WebServer with Java

--- src/test/java/WebServerTest.java
+++ src/test/java/WebServerTest.java
@@ -1,34 +1,51 @@
+import org.junit.jupiter.api.Test;
+
 import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WebServerTest {
 
-    @org.junit.jupiter.api.Test
-    void main() {
+    String WEB_ROOT = (".");
+    String HTML_FILE = ("src/main/java/WebFiles/index.html");
+    String JSON_FILE = ("src/main/java/WebFiles/conFig.json");
+    int PORT = 8080;
+    final boolean verbose = true;
+    final Socket connect = new Socket();
 
-        String WEB_ROOT = (".");
-        String HTML_FILE = ("src/main/java/WebFiles/index.html");
-        String JSON_FILE = ("src/main/java/WebFiles/conFig.json");
-        int PORT = 8080;
-        final boolean verbose = true;
-        final Socket connect = new Socket();
+       @Test
+       void portChecking() {
 
-        assertEquals(8080, PORT);
-        assertNotEquals(7070, PORT);
+           assertEquals(8080, PORT);
+           assertNotEquals(7070, PORT);
+       }
 
-        assertEquals(WEB_ROOT, ".");
-        assertNotEquals("..", WEB_ROOT);
+        @Test
 
-        assertEquals(HTML_FILE, "src/main/java/WebFiles/index.html");
-        assertNotEquals("src/main/java/WebFiles/index.html1", HTML_FILE);
+        void webRootChecking() {
+            assertEquals(WEB_ROOT, ".");
+            assertNotEquals("..", WEB_ROOT);
+        }
 
-        assertEquals(JSON_FILE, "src/main/java/WebFiles/conFig.json");
-        assertNotEquals("src/main/java/WebFiles.conFigs.json", JSON_FILE);
+        @Test
+        void htmlFileChecking() {
+            assertEquals(HTML_FILE, "src/main/java/WebFiles/index.html");
+            assertNotEquals("src/main/java/WebFiles/index.html1", HTML_FILE);
+        }
+
+        @Test
+        void jsonFileChecking() {
+
+            assertEquals(JSON_FILE, "src/main/java/WebFiles/conFig.json");
+            assertNotEquals("src/main/java/WebFiles.conFigs.json", JSON_FILE);
+        }
+
+        @Test
+        void socketChecking() {
 
         assertEquals(connect, connect);
 
-        assertTrue(verbose);
+        assertTrue(verbose, "is True");
 
 
     }
